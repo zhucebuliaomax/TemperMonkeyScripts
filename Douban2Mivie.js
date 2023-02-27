7d46277802ede41c80e763cc8d719929eacb51ca
zhucebuliaomax <tooclose@live.cn>
2023-02-27 15:15:45 +0800

    Update Douban2Mivie.js

--- Douban2Mivie.js
+++ Douban2Mivie.js
@@ -2,11 +2,12 @@
 // @name           Douban2Mivie
 // @name:en        Douban2Mivie
 // @namespace      https://chat.openai.com/
-// @version        0.9.9
+// @version        0.9.10
 // @author         ChatGPT & ilovemonkey
 // @description    在豆瓣电影详情页加入搜索按钮，可以直接跳转至Mivie搜索资源。Written by ChatGPT, modified by ilovemonkey.
 // @description:en Add a search button above Douban's movie info, allowing users to search for related resources on Mivie. Written by ChatGPT, modified by ilovemonkey.
 // @match          https://movie.douban.com/subject/*
+// @match          https://m.douban.com/movie/subject/*
 // @license        MIT
 // @grant          none
 // ==/UserScript==
@@ -76,5 +77,13 @@
   button.addEventListener('click', () => window.open(searchUrl, '_blank'));
 
   const info = document.getElementById('info');
-  info.insertBefore(button, info.firstElementChild);
+  const subMeta = document.querySelector('.sub-meta');
+
+  if (info) {
+    info.insertBefore(button, info.firstElementChild);
+  }
+  // 如果没有 id="info" 的元素，则添加到 class="sub-meta" 的元素的后面
+  else if (subMeta) {
+    subMeta.parentNode.insertBefore(button, subMeta.nextSibling);
+  }
 })();
